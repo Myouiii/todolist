b7221954f63a9f46bfb5902475a443b6ad2554b7
myoui <mypenguinmina@gmail.com>
2023-02-14 00:25:58 +0700

    todolist

---
+++ src/components/Button.js
@@ -0,0 +1,34 @@
+import { getClasses } from "../utils/getClasses";
+import styles from '../styles/modules/button.module.scss';
+
+
+const buttonTypes = {
+    primary: "primary",
+    secondary: "secondary",
+  };
+export default function Button({ children, type, variant, ...rest }) {
+    return (
+        <button
+            className={
+                getClasses([styles.button, styles[`button--${buttonTypes[variant]}`]])
+            }
+            type={type === "submit" ? "submit" : "button"}
+            {...rest}
+        >
+            {children}
+        </button>
+    )
+};
+
+function SelectButton({ children, ...rest }) {
+    return (
+        <select
+        className={getClasses([styles.button, styles.button__select])}
+        {...rest}
+        >
+            {children}
+        </select>
+    )
+};
+
+export { SelectButton };
